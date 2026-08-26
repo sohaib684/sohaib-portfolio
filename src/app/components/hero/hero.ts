edec8352aaafc8a2d6140ba01d9f16af6e910ec7
Sohaib Alam <sohaibalam27022001@gmail.com>
2026-08-26 18:51:13 +0530

    photo and sub-title rotate added

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {}
+export class Hero implements OnInit, OnDestroy {
+
+  currentRole = signal('Java Full Stack Developer');
+
+  roles = [
+    'Java Full Stack Developer',
+    'Software Development Engineer',
+    'Problem Solver'
+  ];
+
+  private roleIndex = 0;
+  private roleInterval?: ReturnType<typeof setInterval>;
+
+  ngOnInit(): void {
+    this.roleInterval = setInterval(() => {
+      this.roleIndex = (this.roleIndex + 1) % this.roles.length;
+      this.currentRole.set(this.roles[this.roleIndex]);
+    }, 2500);
+  }
+
+  ngOnDestroy(): void {
+    if (this.roleInterval) {
+      clearInterval(this.roleInterval);
+    }
+  }
+}
