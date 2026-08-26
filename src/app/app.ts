244b0b332d8f64ea35a600ebc236ec6b755b724d
Sohaib Alam <sohaibalam27022001@gmail.com>
2026-08-26 17:07:30 +0530

    file created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Experience } from './components/experience/experience';
+import { Hero } from './components/hero/hero';
+import { Navbar } from './components/navbar/navbar';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Hero, About, Skills, Experience, Projects, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
